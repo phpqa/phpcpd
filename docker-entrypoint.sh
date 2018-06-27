7b0d90beaaf8ac082469c4fcbb1b2ab2f0b3503e
MetalArend <metalarend@gmail.com>
2018-06-28 00:12:02 +0200

    enhance the tests and variables used

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-if [ "${1:0:1}" = "-" ]; then
-  set -- /sbin/tini -- php /vendor/bin/phpcpd "$@"
-elif [ "$1" = "/vendor/bin/phpcpd" ]; then
+if [ "$(printf %c "$1")" = '-' ]; then
+  set -- /sbin/tini -- php /composer/vendor/bin/phpcpd "$@"
+elif [ "$1" = "/composer/vendor/bin/phpcpd" ]; then
   set -- /sbin/tini -- php "$@"
 elif [ "$1" = "phpcpd" ]; then
-  set -- /sbin/tini -- php /vendor/bin/"$@"
+  set -- /sbin/tini -- php /composer/vendor/bin/"$@"
 elif [ -d "$1" ]; then
-  set -- /sbin/tini -- php /vendor/bin/phpcpd "$@"
+  set -- /sbin/tini -- php /composer/vendor/bin/phpcpd "$@"
 fi
 
 exec "$@"
